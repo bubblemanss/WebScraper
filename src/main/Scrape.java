97a281cbcd574761128371cd81353546f750ded9
ronnh <ronnie.huang@hotmail.ca>
2015-05-12 22:59:28 -0400

    Added option for Mangahere
    Shows the hot new manga updates

--- src/main/Scrape.java
+++ src/main/Scrape.java
@@ -7,22 +7,26 @@ public class Scrape{
   public static void main(String[] args){
     try{
         UserAgent userAgent = new UserAgent();                       //create new userAgent (headless browser).
-        String mangaSite = "http://mangastream.com";  
+        String mangaSite = "http://mangahere.com";  
         ArrayList<String> newTitles = new ArrayList<String>(); 
         if (mangaSite.toLowerCase().contains("mangastream"))                    //visit a ur
         {
     		String searchQuery = "featurebox-caption";
             newTitles = mangaStream(userAgent, mangaSite, searchQuery);
+        }else if(mangaSite.toLowerCase().contains("mangahere")){
+        	String searchQuery = "popular_img";
+        	newTitles = mangaHere(userAgent, mangaSite, searchQuery);
+        	newTitles = spaceFixer(newTitles);        	
         }
         for (String titles : newTitles){
-            System.out.println(titles);
+            System.out.print(titles);
         }
     }
     catch(Exception es){         //if an HTTP/connection error occurs, handle JauntException.
       System.err.println(es);
     }
   }
-
+  
 	  public static String trim(String manga){
 	  	String [] content = manga.split(">"); //essentially splitting by both < and > leaving only content 
 	  	if (content.length > 1)
@@ -56,5 +60,35 @@ public class Scrape{
       		return null;
     	}
 
+	  }
+	  
+	  public static ArrayList<String> mangaHere(UserAgent userAgent, String mangaSite, String searchQuery){
+		  try{
+			  userAgent.visit(mangaSite);
+			  String html = userAgent.doc.innerHTML();
+			  String[] s = html.split("<");
+			  ArrayList<String> newTitles = new ArrayList<String>();
+			  
+			  for(int i = 0; i < s.length; i++){
+				  if(s[i].toLowerCase().contains(searchQuery.toLowerCase())){
+					  newTitles.add(trim(s[i + 4]));
+					  i = i + 4;
+				  }
+			  }
+			  return newTitles;
+		  }
+		  catch(JauntException e){         //if an HTTP/connection error occurs, handle JauntException.
+	      		System.err.println(e);
+	      		return null;
+	    	}
+	  }
+	  
+	  public static ArrayList<String> spaceFixer(ArrayList<String> newTitles){
+		  ArrayList<String> newerTitles = new ArrayList<String>();
+		  for(String title: newTitles){
+			  String new_title = title.trim() + "\n";
+			  newerTitles.add(new_title);
+		  }
+		  return newerTitles;
 	  }
  }
